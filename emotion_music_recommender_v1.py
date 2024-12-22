6fdf7833b3762484adaee25a2d01d0f51f593705
Hariesh Ramesh <harieshkai@gmail.com>
2024-12-22 19:23:34 +0530

    Update emotion_music_recommender_v1.py

--- emotion_music_recommender_v1.py
+++ emotion_music_recommender_v1.py
@@ -21,7 +21,8 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 @st.cache_resource
 def load_model_and_encoder():
     model = keras.models.load_model('./models/mediapipe_3emotion_model_1.h5')
-    le = joblib.load('./Label_Encoder/label_encoder_3_emotion.pkl')
+    model_path = os.path.join(os.getcwd(), 'Label_Encoder', 'label_encoder_3_emotion.pkl')
+    le = joblib.load(model_path)
     return model, le
 
 model, le = load_model_and_encoder()
